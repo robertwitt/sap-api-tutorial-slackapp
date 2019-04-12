44def281540c1470b0b4d8fa8d90f470c0ca06fc
Robert Witt <robert.witt.rw@gmail.com>
2019-04-12 04:39:28 +0000

    Add AJAX call to Slack API.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,10 +5,52 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("de.robertwitt.tutorial.api.slackapp.controller.View1", {
+
 		onInit: function () {
 			var oMessages = new JSONModel();
 			this.getView().setModel(oMessages, "messages");
+
+			var that = this;
+			var oSlackModel = new JSONModel();
+			oSlackModel.loadData("model/slack_credentials.json");
+			oSlackModel.attachRequestCompleted(function() {
+				that._loadChannelMessages();
+			});
+			this.getView().setModel(oSlackModel, "slack");
+		},
+
+		_loadChannelMessages: function () {
+			var oView = this.getView();
+			oView.setBusy(true);
+
+			var self = this;
+
+			var oSlackModel = this.getView().getModel("slack");
+			var channel = oSlackModel.getProperty("/channel");
+			var token = oSlackModel.getProperty("/token");
+
+			$.ajax({
+					type: 'GET',
+					url: "/slack/channels.history?channel=" + channel + "&token=" + token,
+					async: false
+				}).done(function (results) {
+					console.log(results);
+					self.getView().getModel("messages").setProperty("/data", results.messages);
+					oView.setBusy(false);
+				})
+				.fail(function (err) {
+					oView.setBusy(false);
+					if (err !== undefined) {
+						var oErrorResponse = $.parseJSON(err.responseText);
+						sap.m.MessageToast.show(oErrorResponse.message, {
+							duration: 6000
+						});
+					} else {
+						sap.m.MessageToast.show("Unknown error!");
+					}
+				});
 		}
+
 	});
 
 });
