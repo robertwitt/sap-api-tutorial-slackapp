4df5f5a3d8d8567f544300ac3072711dd92869df
Robert Witt <robert.witt.rw@gmail.com>
2019-04-12 04:05:43 +0000

    Add a table to the view and init the model in the controller.

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("de.robertwitt.tutorial.api.slackapp.controller.View1", {
 		onInit: function () {
-
+			var oMessages = new JSONModel();
+			this.getView().setModel(oMessages, "messages");
 		}
 	});
+
 });
